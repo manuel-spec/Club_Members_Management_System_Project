03f29a5dd2b4b37327bb89e6d387f72cf19082cd
manuel-spec <amaniase084@gmail.com>
2023-11-27 10:32:19 +0300

    register for an event successful

--- views/registeredEvents.php
+++ views/registeredEvents.php
@@ -1,9 +1,14 @@
 <?php
 session_start();
 include_once 'inc/navbar.php';
+include_once '../models/users.php';
 if (!isset($_SESSION['username'])) {
     header("location: auth/logout.php");
 }
+$userEvents = new Users();
+// $itr = new ArrayIterator($userEvents->getActiveEvents($_SESSION['username']));
+// var_dump($userEvents->getActiveEvents($_SESSION['username'])[1]['event_title']);
+
 
 ?>
 <!DOCTYPE html>
@@ -17,7 +22,25 @@ if (!isset($_SESSION['username'])) {
 </head>
 
 <body>
+    <div class="container mx-auto my-10 sm:px-20 flex justify-center">
 
+        <!--  Post  -->
+        <div class="rounded overflow-hidden border  bg-white w-1/2 md:mx-0 lg:mx-0 ">
+            <h1 class=" text-3xl text-center">active events</h1>
+            <hr>
+            <div>
+
+                <?php
+                for ($i = 0; $i < count($userEvents->getActiveEvents($_SESSION['username'])); $i++) {
+                    echo "<div class='border p-4'>" . $userEvents->getActiveEvents($_SESSION['username'])[$i]['event_title'] . "</div>";
+                }
+
+                ?>
+            </div>
+        </div>
+
+
+    </div>
 </body>
 
 </html>
